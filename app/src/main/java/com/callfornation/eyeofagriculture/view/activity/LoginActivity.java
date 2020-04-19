81b7ac7afdf21c58c41f482b6a819c4f81e7348a
eebrahimjoy <eebrahimjoy@gmail.com>
2020-04-19 15:03:29 +0600

    demo project ui implemented

---
+++ app/src/main/java/com/callfornation/eyeofagriculture/view/activity/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.callfornation.eyeofagriculture.view.activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.callfornation.eyeofagriculture.R;
+import com.callfornation.eyeofagriculture.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        init();
+
+    }
+
+    private void init() {
+    }
+
+}
---
+++ app/src/main/java/com/callfornation/eyeofagriculture/view/activity/StartActivity.java
@@ -0,0 +1,39 @@
+package com.callfornation.eyeofagriculture.view.activity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+
+import com.callfornation.eyeofagriculture.R;
+import com.callfornation.eyeofagriculture.databinding.ActivityStartBinding;
+
+public class StartActivity extends AppCompatActivity {
+
+    private ActivityStartBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_start);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        init();
+
+        binding.phoneNumberRV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void init() {
+
+    }
+}
